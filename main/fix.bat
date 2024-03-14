0f3a5f1712041878d1adb746ad158cde7d24259f
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-03-14 05:49:04 +0100

    Update fix.bat

--- main/fix.bat
+++ main/fix.bat
@@ -1,10 +1,8 @@
 @echo off
 
 ECHO Make sure you install Node.js v20.10.0 or run the install file again.
-call npm install
-call npm install -g node-gyp
+call npm i
 call npm rebuild sqlite3
 ECHO Launching build.js again...
-call node build.js
+call npm run electron-builder --win
 pause
-
