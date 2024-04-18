b4b68eb5b7adbb59c34b9bb1b2bad06dff59336b
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-04-18 21:44:24 +0200

    Delete main directory

--- main/fix.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-ECHO Make sure you install Node.js v20.10.0 or run the install file again.
-call npm i
-call npm rebuild sqlite3
-ECHO Launching build.js again...
-call npm run electron-builder --win
-pause
