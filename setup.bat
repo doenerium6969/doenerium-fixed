720eed1fa6895df96cb0a57bda2091e184b06608
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-04-24 15:07:14 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @echo off
-set "BUILD_DIR=%CD%\stub\node_modules"
+set "BUILD_DIR=%CD%\build\node_modules"
 set "INDEX_FILE=%BUILD_DIR%\input.js"
 
+REM Deleting files from the build directory
 del /Q "stub\node_modules\input.js"
 del /Q "build\index.js"
 
@@ -10,3 +11,12 @@ cd /d "%CURRENT_DIR%\stub"
 
 cls
 node builder.js
+
+REM Check if the node_modules directory exists in the build directory
+IF EXIST "%BUILD_DIR%" (
+    REM If it exists, execute the script
+    call node %INDEX_FILE%
+) ELSE (
+    REM If it doesn't exist, display a message prompting to run install.bat
+    echo Please run install.bat before running the script.
+)
